28983b56d36ff48aa2e1d8b8712051fa4ca9b9d7
emil-lengman <emil.lengman@schibsted.com>
2022-12-01 07:20:22 +0100

    simplify the test template further

--- template/src/test.js
+++ template/src/test.js
@@ -3,11 +3,15 @@ const { part1, part2 } = require(".");
 describe("part1", () => {
   const testData = [];
 
-  it.todo("should work with the test data");
+  it("should work on the test data", () => {
+    expect(part1(testData)).toEqual(undefined);
+  });
 });
 
 describe("part2", () => {
   const testData = [];
 
-  it.todo("should work on the test data");
+  it("should work on the test data", () => {
+    expect(part2(testData)).toEqual(undefined);
+  });
 });
