f316f907653f0ebd6067d063620b24d0545db259
emil-lengman <emil.lengman@schibsted.com>
2022-12-02 08:51:06 +0100

    udpate template a bit

--- template/src/index.js
+++ template/src/index.js
@@ -2,14 +2,13 @@ const fs = require("fs");
 
 const buffer = fs.readFileSync("./src/input.txt");
 const string = buffer.toString();
-const renameMe = string.split("\n").map((row) => row.trim());
 
 const part1 = (input) => {};
 const part2 = (input) => {};
 
 module.exports = {
-  part1result: part1(renameMe),
-  part2result: part2(renameMe),
+  part1result: part1(string),
+  part2result: part2(string),
   part1,
   part2,
 };
