f74148012b305e5fcac37d8ab71c2019184a35f7
emil-lengman <emil.lengman@schibsted.com>
2022-12-02 09:31:48 +0100

    clean it up

--- 2022/02/src/index.js
+++ 2022/02/src/index.js
@@ -4,59 +4,40 @@ const buffer = fs.readFileSync("./src/input.txt");
 const string = buffer.toString();
 
 const part1 = (input) => {
-  const responseScores = {
-    X: 1,
-    Y: 2,
-    Z: 3,
-  };
-
   const outcomes = {
-    "A X": 3,
-    "A Y": 6,
-    "A Z": 0,
-    "B X": 0,
-    "B Y": 3,
-    "B Z": 6,
-    "C X": 6,
-    "C Y": 0,
-    "C Z": 3,
+    "A X": 3 + 1,
+    "A Y": 6 + 2,
+    "A Z": 0 + 3,
+    "B X": 0 + 1,
+    "B Y": 3 + 2,
+    "B Z": 6 + 3,
+    "C X": 6 + 1,
+    "C Y": 0 + 2,
+    "C Z": 3 + 3,
   };
   return input
     .split("\n")
-    .map((row) => {
-      const [_, response] = row.split(" ");
-
-      const score = outcomes[row] + responseScores[response];
-      return score;
-    })
+    .map((row) => outcomes[row])
     .filter((score) => Number.isInteger(score))
     .reduce((acc, score) => acc + score, 0);
 };
 
 const part2 = (input) => {
-  const responseScores = {
-    ROCK: 1,
-    PAPER: 2,
-    SCISSORS: 3,
+  const outcomes = {
+    "A X": 0 + 3,
+    "A Y": 3 + 1,
+    "A Z": 6 + 2,
+    "B X": 0 + 1,
+    "B Y": 3 + 2,
+    "B Z": 6 + 3,
+    "C X": 0 + 2,
+    "C Y": 3 + 3,
+    "C Z": 6 + 1,
   };
 
-  const outcomes = {
-    "A X": 0 + responseScores["SCISSORS"],
-    "A Y": 3 + responseScores["ROCK"],
-    "A Z": 6 + responseScores["PAPER"],
-    "B X": 0 + responseScores["ROCK"],
-    "B Y": 3 + responseScores["PAPER"],
-    "B Z": 6 + responseScores["SCISSORS"],
-    "C X": 0 + responseScores["PAPER"],
-    "C Y": 3 + responseScores["SCISSORS"],
-    "C Z": 6 + responseScores["ROCK"],
-  };
   return input
     .split("\n")
-    .map((row) => {
-      const score = outcomes[row];
-      return score;
-    })
+    .map((row) => outcomes[row])
     .filter((score) => Number.isInteger(score))
     .reduce((acc, score) => acc + score, 0);
 };
