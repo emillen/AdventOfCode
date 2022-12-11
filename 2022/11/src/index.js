c70c6b8a71504947b1c52d1d0a0fe263529dec9d
emil-lengman <emil.lengman@schibsted.com>
2022-12-11 11:03:52 +0100

    some cleanup

--- 2022/11/src/index.js
+++ 2022/11/src/index.js
@@ -8,7 +8,7 @@ const getMonkeys = (input) => {
     .trim()
     .split("\n\n")
     .map((monkey) => {
-      return monkey.split("\n").reduce((acc, line, index) => {
+      return monkey.split("\n").reduce((acc, line, index, self) => {
         switch (index) {
           case 0:
             return { ...acc, id: line.split(" ")[1].slice(0, -1) };
@@ -25,24 +25,36 @@ const getMonkeys = (input) => {
             const argIsNum = !isNaN(parseInt(arg));
             return {
               ...acc,
-              operation: { op, arg: argIsNum ? parseInt(arg) : null },
+              operation: (n) => {
+                switch (op) {
+                  case "*":
+                    return n * (argIsNum ? parseInt(arg) : n);
+                  case "+":
+                    return n + (argIsNum ? parseInt(arg) : n);
+                }
+              },
             };
           case 3:
             const [disibleBy] = line.split(" ").reverse();
             return { ...acc, divisibleBy: parseInt(disibleBy) };
           case 4:
-          case 5:
-            const word = line.split(" ").filter((word) => word !== "");
+            const lineAfter = self[index + 1];
 
-            const [, when] = word;
-            const [throwTo] = word.reverse();
+            const [throwToIfTrue] = line.split(" ").reverse();
+            const [throwToIfFalse] = lineAfter.split(" ").reverse();
 
-            const capitlizeWhen =
-              when[0].toUpperCase() + when.slice(1, when.length - 1);
+            const parsedThrowToIfTrue = parseInt(throwToIfTrue);
+            const parsedThrowToIfFalse = parseInt(throwToIfFalse);
 
             return {
               ...acc,
-              [`when${capitlizeWhen}`]: { throwTo: parseInt(throwTo) },
+              getMonkeyToThrowTo: (n) => {
+                if (n % acc.divisibleBy === 0) {
+                  return parsedThrowToIfTrue;
+                }
+
+                return parsedThrowToIfFalse;
+              },
             };
           default:
             return acc;
@@ -53,81 +65,27 @@ const getMonkeys = (input) => {
   return monkeys;
 };
 
-const getMonkeyToThrowTo = (monkey, item) => {
-  if (item % monkey.divisibleBy === 0) {
-    return monkey.whenTrue.throwTo;
-  }
-
-  return monkey.whenFalse.throwTo;
-};
-
-const part1 = (input) => {
-  const monkeys = getMonkeys(input);
-  const monkeyBusiness = Array(20).fill(0);
-
-  for (let round = 0; round < 20; round++) {
-    for (const monkey of monkeys) {
-      for (
-        let item = monkey.items.pop();
-        item !== undefined;
-        item = monkey.items.pop()
-      ) {
-        monkeyBusiness[monkey.id] = monkeyBusiness[monkey.id] + 1;
-
-        let worryLevel = item;
-        const arg = monkey.operation.arg ? monkey.operation.arg : worryLevel;
-
-        switch (monkey.operation.op) {
-          case "*":
-            worryLevel = worryLevel * arg;
-            break;
-          case "+":
-            worryLevel = worryLevel + arg;
-            break;
-        }
-        worryLevel = Math.floor(worryLevel / 3);
-
-        const monkeyToThrowTo = getMonkeyToThrowTo(monkey, worryLevel);
-
-        monkeys[monkeyToThrowTo].items.push(worryLevel);
-      }
-    }
-  }
-
-  return monkeyBusiness
-    .sort((a, b) => b - a)
-    .slice(0, 2)
-    .reduce((acc, elem) => acc * elem, 1);
-};
-const part2 = (input) => {
-  const monkeys = getMonkeys(input);
+const doMonkeyBusiness = ({ monkeys, withRelief = false, rounds }) => {
   const monkeyBusiness = Array(20).fill(0);
 
   const divisor = monkeys.reduce((acc, monkey) => {
     return acc * monkey.divisibleBy;
   }, 1);
 
-  for (let round = 0; round < 10000; round++) {
+  for (let round = 0; round < rounds; round++) {
     for (const monkey of monkeys) {
       for (
-        let item = monkey.items.pop();
-        item !== undefined;
-        item = monkey.items.pop()
+        let worryLevel = monkey.items.pop();
+        worryLevel !== undefined;
+        worryLevel = monkey.items.pop()
       ) {
         monkeyBusiness[monkey.id] = monkeyBusiness[monkey.id] + 1;
 
-        let worryLevel = item;
-        const arg = monkey.operation.arg ? monkey.operation.arg : worryLevel;
+        worryLevel = monkey.operation(worryLevel);
 
-        switch (monkey.operation.op) {
-          case "*":
-            worryLevel = worryLevel * arg;
-            break;
-          case "+":
-            worryLevel = worryLevel + arg;
-            break;
-        }
-        const monkeyToThrowTo = getMonkeyToThrowTo(monkey, worryLevel);
+        worryLevel = withRelief ? Math.floor(worryLevel / 3) : worryLevel;
+
+        const monkeyToThrowTo = monkey.getMonkeyToThrowTo(worryLevel);
 
         monkeys[monkeyToThrowTo].items.push(worryLevel % divisor);
       }
@@ -138,6 +96,17 @@ const part2 = (input) => {
     .sort((a, b) => b - a)
     .slice(0, 2)
     .reduce((acc, elem) => acc * elem, 1);
+};
+
+const part1 = (input) => {
+  const monkeys = getMonkeys(input);
+
+  return doMonkeyBusiness({ monkeys, withRelief: true, rounds: 20 });
+};
+const part2 = (input) => {
+  const monkeys = getMonkeys(input);
+
+  return doMonkeyBusiness({ monkeys, rounds: 10000 });
 };
 
 module.exports = {
