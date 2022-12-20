8578254be9c1fce163824e93e6704808861f98d0
emil-lengman <emil.lengman@schibsted.com>
2022-12-20 16:48:23 +0100

    update template with comments to ease the start haha

--- template/src/index.js
+++ template/src/index.js
@@ -3,8 +3,13 @@ const fs = require("fs");
 const buffer = fs.readFileSync("./src/input.txt");
 const string = buffer.toString();
 
-const part1 = (input) => {};
-const part2 = (input) => {};
+const part1 = (input) => {
+  /* lets go */
+};
+
+const part2 = (input) => {
+  /* lets go */
+};
 
 module.exports = {
   part1result: () => part1(string),
